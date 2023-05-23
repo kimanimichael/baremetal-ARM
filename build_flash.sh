77bbb3bb624a00fa60619100d78ff7a96fa385be
Michael Kimani <michael@novek.io>
2023-05-23 10:37:35 +0300

    Rename registers

--- build_flash.sh
+++ build_flash.sh
@@ -1,4 +1,6 @@
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -c -o delay.o delay.c 
 arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -c -o main.o main.c 
-arm-none-eabi-ld -T linker.ld -o main.elf main.o
+# arm-none-eabi-ld -Map main.map -T linker.ld -o main.elf main.o
+arm-none-eabi-ld -T linker.ld -o main.elf delay.o main.o
 arm-none-eabi-objcopy -O binary main.elf main.bin
 st-flash write main.bin 0x08000000
