59abb374e434e25aa72f52febc34a6f1a04b9826
MK <mikalkimmani@gmail.com>
2023-08-22 06:02:20 +0300

    (lesson16): Cleanup

--- main.c
+++ main.c
@@ -6,72 +6,21 @@
 #define GPIOB_MODER (*((unsigned int *)(0x40020400)))
 #define GPIOB_ODR (*((unsigned int *)(0x40020414)))
 /**
- * @brief point represents a struct that has members x and y
+ * @brief main program function
  * @author @Mike-Kimani
 */
-typedef struct 
-{
-    long x;
-    long y;
-} point;
-
-point p1, p2;
-typedef struct
-{
-    /* data */
-    point p, q;
-} player;
-
-player mk;
-
-/**
- * @bug Unexplained behaviour with struct below
-*/
-typedef struct 
-{
-    /* data */
-    point corners[4];
-} rectangle;
-
-rectangle rm;
-
 int main()
 {
-    mk.p.x = 10; //mk position p
-    mk.p.y = 20; //mk position p
-
-    mk.q.x = 30; //mk position q
-    mk.q.y = 45; //mk position q
-
-    rm.corners[0].x = 0;
-    rm.corners[0].y = 0;
-    rm.corners[1].x = 10;
-    rm.corners[1].y = 0;
-    rm.corners[2].x = 10;
-    rm.corners[2].y = 5;
-    rm.corners[3].x = 0;
-    rm.corners[3].y = 5;
-    
-    p1.x = 1000000;
-    p1.y = 2000000;
-    p2 = p1;
-
-    point *pp;
-    player *pl;
-
-    pp = &p1;
-    pl = &mk;
-
     RCC_AH1BEN |= 0x2;
     GPIOB_MODER |= (0b01 << 14);
 
     while (1)
     {
         GPIOB_ODR |= (0b01 << 7);
-        delay((*pp).x);
+        delay(1000000);
 
         GPIOB_ODR &= ~(0b1 << 7);
-        delay((*pp).y);
+        delay(2000000);
     }
     
 }
