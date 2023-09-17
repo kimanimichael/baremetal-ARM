275a676c7bfd31ddd70df93dca8cf5dd8f07c3c4
MK <mikalkimmani@gmail.com>
2023-09-17 11:11:22 +0300

    (lesson16): Include folder restructure

--- src/bsp.c
+++ src/bsp.c
@@ -1,5 +1,5 @@
 #include "../include/defs.h"
-#include "../include/stm32f429xx.h"
+#include "../include/cmsis/stm32f429xx.h"
 
 void assert_failed(char const* file, int line) {
     /**
--- startup/startup.c
+++ startup/startup.c
@@ -1,6 +1,6 @@
 #include "../include/defs.h"
-#include "../include/stm32f429xx.h"
-#include "../include/stm32f4xx_it.h"
+#include "../include/cmsis/stm32f429xx.h"
+#include "../include/cmsis/stm32f4xx_it.h"
 
 extern unsigned int *_data_start;
 extern unsigned int *_data_end;
