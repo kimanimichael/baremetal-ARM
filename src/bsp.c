446ee900a5a4aa209b2cb3dee10c86bac72a9533
MK <mikalkimmani@gmail.com>
2023-10-04 23:06:49 +0300

    (lesson16): Use the systick interrupt for blinking

--- src/bsp.c
+++ src/bsp.c
@@ -1,9 +1,19 @@
 #include "../include/defs.h"
 #include "../include/cmsis/stm32f429xx.h"
+#include "../include/bsp.h"
 
 void assert_failed(char const* file, int line) {
     /**
      * @brief Resets the syste,
     */
     NVIC_SystemReset();
+}
+
+void SysTick_Handler (void) 
+{
+    GPIOx_ODR ^= (0b01 << 7);
+} 
+
+void ledOn() {
+    GPIOx_ODR = (0b01 << 7);
 }
--- src/main.c
+++ src/main.c
@@ -1,19 +1,15 @@
 #include "../include/defs.h"
 #include "../include/delay.h"
 #include "stdint.h"
+#include "../include/cmsis/stm32f429xx.h"
+#include "../include/bsp.h"
 
-//Base RCC register 0x4002 3800 + offset 0x30 to find RCC_AHB1ENR
-#define RCC_AH1BEN (*((unsigned int *)(0x40023830)))
 
-//Base GPIOB register 0x4002 0400 + offset 0x00 to find GPIOB_MODER
-#define GPIOB_MODER (*((unsigned int *)(0x40020400)))
-
-//Base GPIOB register 0x4002 0400 + offset 0x14 to find GPIOx_ODR
-#define GPIOx_ODR (*((unsigned int *)(0x40020414)))
 /**
  * @brief main program function
  * @author @Mike-Kimani
 */
+
 int main()
 {
     //Bitwise OR the second bit of RCC_AHB1ENR with 1 to enable GPIOB_EN
@@ -23,13 +19,16 @@ int main()
     //Bitwise OR the 15th bit of GPIOB_MODER with 1
     GPIOB_MODER |= (0b01 << 14);
     
+    SysTick_Config(SYS_CLCK_HZ/2);
+
+    //Find out why this isn't necessary
+    __enable_irq();
+
+    ledOn();
 
     while (1)
     {
-        //bit-wise XOR the 8th bit in GPIOx_ODR with 1 - This toggles the bit and hence the led
-        GPIOx_ODR ^= (0b01 << 7);
-        delay(1000000);
-
+        delay(500000);
     }
     
 }
