11bc03805f03577b249cc1d43c79124d1cad1af0
MK <mikalkimmani@gmail.com>
2023-10-04 23:06:06 +0300

    (lesson16): bsp header file to define the board specific definitions

---
+++ include/bsp.h
@@ -0,0 +1,17 @@
+#ifndef __BSP__H
+#define __BSP__H
+
+#define SYS_CLCK_HZ 16000000U
+
+//Base RCC register 0x4002 3800 + offset 0x30 to find RCC_AHB1ENR
+#define RCC_AH1BEN (*((unsigned int *)(0x40023830)))
+
+//Base GPIOB register 0x4002 0400 + offset 0x00 to find GPIOB_MODER
+#define GPIOB_MODER (*((unsigned int *)(0x40020400)))
+
+//Base GPIOB register 0x4002 0400 + offset 0x14 to find GPIOx_ODR
+#define GPIOx_ODR (*((unsigned int *)(0x40020414)))
+
+void ledOn();
+
+#endif
