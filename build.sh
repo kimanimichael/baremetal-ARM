b854561b14a3dbf8635d2fbf8d956f0ccec4511e
MK <muchunumike@gmail.com>
2024-07-27 21:24:07 +0300

    (lesson22): Add script for building without flashing

---
+++ build.sh
@@ -0,0 +1,14 @@
+rm -rf build/
+mkdir build/
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -c -o build/startup.o startup/startup.c
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -c -o build/bsp.o src/bsp.c
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -c -o build/delay.o src/delay.c
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -c -o build/system_stm32f4xx.o core/system_stm32f4xx.c
+arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -c -o build/main.o src/main.c
+# arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/system_stm32f4xx.o build/main.o
+arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/main.o
+arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o main.elf build/startup.o build/bsp.o build/delay.o build/main.o
+
+
+# arm-none-eabi-ld -T linker.ld -o main.elf delay.o main.o
+arm-none-eabi-objcopy -O binary build/main.elf build/main.bin
