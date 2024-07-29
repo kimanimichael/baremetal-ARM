4597a368ad6ffb65d8e42503d25051adad24d4f3
MK <muchunumike@gmail.com>
2024-07-29 21:15:37 +0300

    (lesson22): Add function prototypes and remove stack boudary alignment

--- src/main.c
+++ src/main.c
@@ -12,8 +12,12 @@
 
 #ifdef blocking
 
-uint32_t stack_blinky1[40]__attribute__ ((aligned (8)));
+uint32_t stack_blinky1[40];
 uint32_t *sp_blinky1 = &stack_blinky1[40];
+
+void main_blinky1();
+void main_blinky2();
+
 
 void main_blinky1() {
     while (1)
@@ -23,8 +27,9 @@ void main_blinky1() {
     }
 
 }
-uint32_t stack_blinky2[40]__attribute__ ((aligned (8)));
+uint32_t stack_blinky2[40];
 uint32_t *sp_blinky2 = &stack_blinky2[40];
+
 void main_blinky2() {
     while (1)
     {
@@ -37,6 +42,8 @@ void main_blinky2() {
 int main() {
     BSP_init();
     BSP_ledInit();
+    BSP_blueLedOn();
+//    main_blinky2();
 
     /* fabricate Cortex-M ISR stack frame for blinky 1*/
     *(--sp_blinky1) = (1U << 24); /* xPSR */
@@ -58,70 +65,13 @@ int main() {
     *(--sp_blinky2) = 0x00000001U; /* R1 */
     *(--sp_blinky2) = 0x00000000U; /* R0 */
 //
+    BSP_redLedOn();
     while (1) {
 
     }
 }
 
-// int main()
-// {
-//     BSP_init();
-//     BSP_ledInit();
 
-//     // Set the thumb state
-//     *(--sp_blinky1) = (1U << 24); /* xPSR */
-//     *(--sp_blinky1) = (uint32_t)&main_blinky1; /*PC*/
-//     *(--sp_blinky1) = 0x0000000EU;
-//     *(--sp_blinky1) = 0x0000000CU;
-//     *(--sp_blinky1) = 0x00000003U;
-//     *(--sp_blinky1) = 0x00000002U;
-//     *(--sp_blinky1) = 0x00000001U;
-//     *(--sp_blinky1) = 0x00000000U;
-
-//     // Save additional registers to prevent cloberring
-//     *(--sp_blinky1) = 0x0000000BU;
-//     *(--sp_blinky1) = 0x0000000AU;
-//     *(--sp_blinky1) = 0x00000009U;
-//     *(--sp_blinky1) = 0x00000008U;
-//     *(--sp_blinky1) = 0x00000007U;
-//     *(--sp_blinky1) = 0x00000006U;
-//     *(--sp_blinky1) = 0x00000005U;
-//     *(--sp_blinky1) = 0x00000004U;
-
-//     // Set the thumb state
-//     *(--sp_blinky2) = (1U << 24); /* xPSR */
-//     *(--sp_blinky2) = (uint32_t)&main_blinky2; /*PC*/
-//     *(--sp_blinky2) = 0x0000000EU;
-//     *(--sp_blinky2) = 0x0000000CU;
-//     *(--sp_blinky2) = 0x00000003U;
-//     *(--sp_blinky2) = 0x00000002U;
-//     *(--sp_blinky2) = 0x00000001U;
-//     *(--sp_blinky2) = 0x00000000U;
-
-//     //Save additional registers to prevent cloberring
-//     *(--sp_blinky2) = 0x0000000BU;
-//     *(--sp_blinky2) = 0x0000000AU;
-//     *(--sp_blinky2) = 0x00000009U;
-//     *(--sp_blinky2) = 0x00000008U;
-//     *(--sp_blinky2) = 0x00000007U;
-//     *(--sp_blinky2) = 0x00000006U;
-//     *(--sp_blinky2) = 0x00000005U;
-//     *(--sp_blinky2) = 0x00000004U;
-
-//     while (1)
-//     {
-//         /* code */
-//     }
-    
-//     // uint32_t volatile run = 0;
-
-//     // if(run) {
-//     //     main_blinky1();
-//     // }
-//     // else {
-//     //     main_blinky2();
-//     // }
-// }
 
 #else
 int main()
