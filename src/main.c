8b240b91c21cad96a3d2c0443ab658a38cbe9690
MK <muchunumike@gmail.com>
2024-07-30 23:41:55 +0300

    (lesson22): Save registers R4-R11 to the stack frame that may be clobbered during the context switch

--- src/main.c
+++ src/main.c
@@ -53,7 +53,15 @@ int main() {
     *(--sp_blinky1) = 0x00000002U; /* R2 */
     *(--sp_blinky1) = 0x00000001U; /* R1 */
     *(--sp_blinky1) = 0x00000000U; /* R0 */
-//
+    /* additional fake registers R4 - R11 */
+    *(--sp_blinky1) = 0x0000000BU; /* R11 */
+    *(--sp_blinky1) = 0x0000000AU; /* R10 */
+    *(--sp_blinky1) = 0x00000009U; /* R9*/
+    *(--sp_blinky1) = 0x00000008U; /* R8 */
+    *(--sp_blinky1) = 0x00000007U; /* R7 */
+    *(--sp_blinky1) = 0x00000006U; /* R6 */
+    *(--sp_blinky1) = 0x00000005U; /* R5 */
+    *(--sp_blinky1) = 0x00000004U; /* R4 */
     /* fabricate Cortex-M ISR stack frame for blinky 2*/
     *(--sp_blinky2) = (1U << 24); /* xPSR */
     *(--sp_blinky2) = (uint32_t)&main_blinky2;
@@ -63,6 +71,15 @@ int main() {
     *(--sp_blinky2) = 0x00000002U; /* R2 */
     *(--sp_blinky2) = 0x00000001U; /* R1 */
     *(--sp_blinky2) = 0x00000000U; /* R0 */
+    /* additional fake registers R4 - R11 */
+    *(--sp_blinky2) = 0x0000000BU; /* R11 */
+    *(--sp_blinky2) = 0x0000000AU; /* R10 */
+    *(--sp_blinky2) = 0x00000009U; /* R9*/
+    *(--sp_blinky2) = 0x00000008U; /* R8 */
+    *(--sp_blinky2) = 0x00000007U; /* R7 */
+    *(--sp_blinky2) = 0x00000006U; /* R6 */
+    *(--sp_blinky2) = 0x00000005U; /* R5 */
+    *(--sp_blinky2) = 0x00000004U; /* R4 */
 //
     while (1) {
 
