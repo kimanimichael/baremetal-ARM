69af4e7ccd90c051745025f3989eb0329b33d811
MK <muchunumike@gmail.com>
2024-08-07 16:38:09 +0300

    lesson23: Lesson23 progress

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ void main_blinky2();
 
 uint32_t stack_blinky1[40];
 OSThread blinky1;
- uint32_t *sp_blinky1 = &stack_blinky1[40];
+// uint32_t *sp_blinky1 = &stack_blinky1[40];
 
 void main_blinky1() {
     while (1)
@@ -29,7 +29,7 @@ void main_blinky1() {
 
 }
 uint32_t stack_blinky2[40];
-uint32_t *sp_blinky2 = &stack_blinky2[40];
+//uint32_t *sp_blinky2 = &stack_blinky2[40];
 
 OSThread blinky2;
 // uint32_t *sp_blinky2 = &stack_blinky2[40];
@@ -47,45 +47,8 @@ int main() {
     OS_init();
     BSP_ledInit();
     BSP_blueLedOn();
+    BSP_blueLedOff();
 //    main_blinky2();
-
-    /* fabricate Cortex-M ISR stack frame for blinky 1*/
-    *(--sp_blinky1) = (1U
-            << 24); /* xPSR */ // Stack pointer decremented as the arm cpu uses the full-stack i.e. stack pointer points to last used  stack entry
-    *(--sp_blinky1) = (uint32_t) &main_blinky1;
-    *(--sp_blinky1) = 0x0000000EU; /* LR */
-    *(--sp_blinky1) = 0x0000000CU; /* R12 */
-    *(--sp_blinky1) = 0x00000003U; /* R3 */
-    *(--sp_blinky1) = 0x00000002U; /* R2 */
-    *(--sp_blinky1) = 0x00000001U; /* R1 */
-    *(--sp_blinky1) = 0x00000000U; /* R0 */
-    /* additional fake registers R4 - R11 */
-    *(--sp_blinky1) = 0x0000000BU; /* R11 */
-    *(--sp_blinky1) = 0x0000000AU; /* R10 */
-    *(--sp_blinky1) = 0x00000009U; /* R9*/
-    *(--sp_blinky1) = 0x00000008U; /* R8 */
-    *(--sp_blinky1) = 0x00000007U; /* R7 */
-    *(--sp_blinky1) = 0x00000006U; /* R6 */
-    *(--sp_blinky1) = 0x00000005U; /* R5 */
-    *(--sp_blinky1) = 0x00000004U; /* R4 */
-    /* fabricate Cortex-M ISR stack frame for blinky 2*/
-    *(--sp_blinky2) = (1U << 24); /* xPSR */
-    *(--sp_blinky2) = (uint32_t) &main_blinky2;
-    *(--sp_blinky2) = 0x0000000EU; /* LR */
-    *(--sp_blinky2) = 0x0000000CU; /* R12 */
-    *(--sp_blinky2) = 0x00000003U; /* R3 */
-    *(--sp_blinky2) = 0x00000002U; /* R2 */
-    *(--sp_blinky2) = 0x00000001U; /* R1 */
-    *(--sp_blinky2) = 0x00000000U; /* R0 */
-    /* additional fake registers R4 - R11 */
-    *(--sp_blinky2) = 0x0000000BU; /* R11 */
-    *(--sp_blinky2) = 0x0000000AU; /* R10 */
-    *(--sp_blinky2) = 0x00000009U; /* R9*/
-    *(--sp_blinky2) = 0x00000008U; /* R8 */
-    *(--sp_blinky2) = 0x00000007U; /* R7 */
-    *(--sp_blinky2) = 0x00000006U; /* R6 */
-    *(--sp_blinky2) = 0x00000005U; /* R5 */
-    *(--sp_blinky2) = 0x00000004U; /* R4 */
 //
     while (1) {
 
@@ -102,7 +65,7 @@ int main() {
 
         while (1) {
             /* code */
-            BSP_blueLedOn();
+            BSP_blueLedOff();
         }
     }
 }
