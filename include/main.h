75941e2bb095efbc51e875e4760f897287566335
MK <muchunumike@gmail.com>
2024-08-25 22:59:55 +0300

    (lesson23): Move documentation to header file

--- include/main.h
+++ include/main.h
@@ -1,9 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * @brief main program function
+ * @author @Mike-Kimani
+*/
 int main();
-
+/**
+ * @brief Blink green led
+ * @author @Mike-Kimani
+*/
 void main_blinky1();
+/**
+ * @brief Blink blue led
+ * @author @Mike-Kimani
+*/
 void main_blinky2();
 
 #endif
