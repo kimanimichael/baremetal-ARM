4ca755628762cdc364be3b4f23700e4e1e5c8ff0
MK <muchunumike@gmail.com>
2024-08-27 00:21:10 +0300

    (lesson24): Lint

--- src/main.c
+++ src/main.c
@@ -43,23 +43,23 @@ int main() {
     OS_init();
     BSP_ledInit();
 
-        /* fabricate Cortex-M ISR stack for blinky1 */
-        OSThread_start(&blinky1,
-                       &main_blinky1,
-                       stack_blinky1,
-                       sizeof(stack_blinky1));
-        /* fabricate Cortex-M ISR stack for blinky2 */
-        OSThread_start(&blinky2,
-                       &main_blinky2,
-                       stack_blinky2,
-                       sizeof(stack_blinky2));
-        /* fabricate Cortex-M ISR stack for blinky3 */
-        OSThread_start(&blinky3,
-            &main_blinky3,
-            stack_blinky3,
-            sizeof(stack_blinky3));
+    /* fabricate Cortex-M ISR stack for blinky1 */
+    OSThread_start(&blinky1,
+                   &main_blinky1,
+                   stack_blinky1,
+                   sizeof(stack_blinky1));
+    /* fabricate Cortex-M ISR stack for blinky2 */
+    OSThread_start(&blinky2,
+                   &main_blinky2,
+                   stack_blinky2,
+                   sizeof(stack_blinky2));
+    /* fabricate Cortex-M ISR stack for blinky3 */
+    OSThread_start(&blinky3,
+        &main_blinky3,
+        stack_blinky3,
+        sizeof(stack_blinky3));
 
-        OS_run();
+    OS_run();
 }
 
 
