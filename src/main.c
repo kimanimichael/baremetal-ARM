036a188f232d87eff33252e8815d45893bb1a28b
MK <muchunumike@gmail.com>
2024-08-26 22:43:21 +0300

    (lesson24): Add third thread

--- src/main.c
+++ src/main.c
@@ -29,6 +29,14 @@ void main_blinky2() {
     }
 
 }
+uint32_t stack_blinky3[40];
+OSThread blinky3;
+void main_blinky3() {
+    while (1) {
+        BSP_redLedToggle();
+        BSP_Delay(100);
+    }
+}
 
 int main() {
     BSP_init();
@@ -46,6 +54,11 @@ int main() {
                        &main_blinky2,
                        stack_blinky2,
                        sizeof(stack_blinky2));
+        /* fabricate Cortex-M ISR stack for blinky3 */
+        OSThread_start(&blinky3,
+            &main_blinky3,
+            stack_blinky3,
+            sizeof(stack_blinky3));
 
         while (1) {
             /* code */
