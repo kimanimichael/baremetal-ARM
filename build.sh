0159fdb8ca99b8287c4de9372a023f676e086211
MK <muchunumike@gmail.com>
2024-09-15 21:49:27 +0300

    l25(efficient_thread_blocking): Build System source files

--- build.sh
+++ build.sh
@@ -24,14 +24,14 @@ arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4+nofp -mthumb -c -o build/miros.o 
 arm-none-eabi-gcc -O0 -g -Wall -mcpu=cortex-m4+nofp -mthumb -c -o build/main.o src/main.c
 # arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/system_stm32f4xx.o build/main.o
 
-arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/miros.o build/main.o
-arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o main.elf build/startup.o build/bsp.o build/delay.o build/miros.o build/main.o
+arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/system_stm32f4xx.o build/miros.o build/main.o
+arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o main.elf build/startup.o build/bsp.o build/delay.o build/system_stm32f4xx.o build/miros.o build/main.o
 
 
 # arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/miros.o build/system_stm32f4xx.o build/main.o
 # arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o main.elf build/startup.o build/bsp.o build/delay.o build/miros.o build/system_stm32f4xx.o build/main.o
 
-arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/miros.o build/main.o
+arm-none-eabi-ld -Map build/main.map -T startup/linker.ld -o build/main.elf build/startup.o build/bsp.o build/delay.o build/system_stm32f4xx.o build/miros.o build/main.o
 
 # arm-none-eabi-ld -T linker.ld -o main.elf delay.o main.o
 arm-none-eabi-objcopy -O binary build/main.elf build/main.bin
