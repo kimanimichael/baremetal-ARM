e89f5e6cc029fef8c02c89b8cbda7f9be9cc7ce3
MK <muchunumike@gmail.com>
2024-09-17 12:38:42 +0300

    l25(efficient_thread_blocking): Use OS_functions for thread handling

--- src/bsp.c
+++ src/bsp.c
@@ -21,16 +21,21 @@ void OS_onStartup(void) {
 }
 
 void OS_on_idle(void) {
+    GPIOx_ODR |= (0b01 << 14);
+    GPIOx_ODR &= ~(0b01 << 14);
+
 }
 
 unsigned int volatile l_tickrCtr;
 
 void SysTick_Handler (void) 
 {
-    ++l_tickrCtr;
-    
+    GPIOx_ODR |= (0b01 << 1);
+    OS_tick();
+
     __disable_irq();
     OS_sched();
+    GPIOx_ODR &= ~(0b01 << 1);
     __enable_irq();
 } 
 
@@ -67,10 +72,10 @@ void BSP_init() {
 void BSP_ledInit() {
     //Bitwise OR the second bit of RCC_AHB1ENR with 1 to enable GPIOB_EN CLOCK
     RCC_AH1BEN |= (0b01 << 1);
-    //Bitwise AND the 16th bit and 2nd bit of GPIOB_MODER with 0 - CONFIG PB7 & PB0 & PB14 as output
-    GPIOB_MODER &= ((0b00 << 15) | (0b00 << 1) |(0b00 << 29));
-    //Bitwise OR the 15th bit and 1st of GPIOB_MODER with 1 - CONFIG PB7 & PB0 & PB14 as output
-    GPIOB_MODER |= ((0b01 << 14) | (0b01 << 0) | (0b01 << 28));
+    //Bitwise AND the 16th bit and 2nd bit of GPIOB_MODER with 0 - CONFIG PB7 & PB0 & PB14 & PB1 as output
+    GPIOB_MODER &= ((0b00 << 15) | (0b00 << 1) | (0b00 << 29) | (0b00 << 3));
+    //Bitwise OR the 15th bit and 1st of GPIOB_MODER with 1 - CONFIG PB7 & PB0 & PB14 & PB1 as output
+    GPIOB_MODER |= ((0b01 << 14) | (0b01 << 0) | (0b01 << 28) | (0b01 << 2));
 }
 
 void BSP_greenLedToggle() {
