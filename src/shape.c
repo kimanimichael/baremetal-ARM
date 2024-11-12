360f61f9422ed77a4a8e625e646f00fdba3d3b54
MK <muchunumike@gmail.com>
2024-11-12 14:39:55 +0300

    l32(OOP_C_polymorphism): Init base shape area and draw functions

--- src/shape.c
+++ src/shape.c
@@ -1,5 +1,8 @@
 #include "shape.h"
 
+static void Shape_draw(Shape const * const me);
+static uint32_t Shape_area(Shape const * const me);
+
 void Shape_ctor(Shape* const me, int16_t x0, int16_t y0) {
     me -> x = x0;
     me -> y = y0;
@@ -22,5 +25,18 @@ uint16_t Shape_distance_from(Shape const* const me, Shape const* other) {
     return dx + dy;
 }
 
+void Shape_draw(Shape const * const me) {
+    (void)me;
+}
+uint32_t Shape_area(Shape const * const me) {
+    (void)me;
+    return 0U;
+}
+
+void drawGraph(Shape const * graph[]) {
+    for (uint8_t i = 0; graph[i] != (Shape *)0; i++) {
+        Shape_draw_vcall(graph[i]); /* polymorphism */
+    }
+}
 
 
