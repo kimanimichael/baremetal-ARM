41065b8c14394f801796c7f7263233acb83f38d3
mikael <muchunumike@gmail.com>
2025-06-03 07:43:12 +0300

    l45(software_tracing_with_printf): Ensure floating point initialization

--- core/system_stm32f4xx.c
+++ core/system_stm32f4xx.c
@@ -216,6 +216,11 @@ void SystemInit(void)
   /* FPU settings ------------------------------------------------------------*/
   #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
     SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
+
+  /* Ensure subsequent instructions use updated CPACR immediately */
+  __DSB();
+  __ISB();
+
   #endif
   /* Reset the RCC clock configuration to the default reset state ------------*/
   /* Set HSION bit */
